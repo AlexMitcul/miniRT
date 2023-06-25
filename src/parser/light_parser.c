ca84c91ea85f4f33c85c6ad77451c86d9d4bea52
Alex Mitcul <amitcul@student.42porto.com>
2023-06-25 18:54:42 +0100

    functions description added

--- src/parser/light_parser.c
+++ src/parser/light_parser.c
@@ -1,7 +1,16 @@
 
 #include "../../includes/minirt.h"
 
-/* A 0.2 255,255,255 */
+/*
+ * @ Description:
+ 	* Validate that input string is in right format
+ 	* where format is 'A 0.2 255,255,255' =>
+ 	* char float color_string
+ * @ Input:
+ 	* char **data => strings array of strings represents ambient light data
+ * @ Return value:
+ 	* return true if is valid, false otherwise
+ */
 static bool validate_ambient_light_line(char **data)
 {
 	if (is_float(data[1]) == false || is_color_string(data[2]) == false)
@@ -9,6 +18,17 @@ static bool validate_ambient_light_line(char **data)
 	return (true);
 }
 
+/*
+ * @ Description:
+ 	* Parse input strings array, create ambient light structure and assign it
+ 	* to scene
+ * @ Input:
+ 	* t_scene *scene => scene with all data
+ 	* char **data => strings array represents ambient light data
+ * @ Return value:
+ 	* return EXIT_SUCCESS if all passed good,
+ 	* EXIT_FAILED otherwise
+ */
 int	parse_ambient_light(t_scene *scene, char **data)
 {
 	t_ambient_light *ambient_light;
