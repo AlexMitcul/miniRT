01561087b951bac8a7d536263d79815b4acf99ab
amitcul <amitcul@student.42porto.com>
2023-07-17 11:44:52 +0100

    scene norm fix

--- src/scene/camera.c
+++ src/scene/camera.c
@@ -12,26 +12,26 @@
 
 #include "../../includes/minirt.h"
 
-t_camera    *new_camera(t_vector *origin, t_vector *direction, float fov)
+t_camera	*new_camera(t_vector *origin, t_vector *direction, float fov)
 {
-    t_camera *camera;
+	t_camera	*camera;
 
-    camera = (t_camera *) malloc(sizeof(t_camera));
-    if (!camera)
-        handle_error(MEMORY_ERROR, NULL);
-    camera->origin = origin;
-    camera->direction = direction;
-    camera->fov = fov;
-    return (camera);
+	camera = (t_camera *)malloc(sizeof(t_camera));
+	if (!camera)
+		handle_error(MEMORY_ERROR, NULL);
+	camera->origin = origin;
+	camera->direction = direction;
+	camera->fov = fov;
+	return (camera);
 }
 
-void    free_camera(t_camera *camera)
+void	free_camera(t_camera *camera)
 {
-    if (!camera)
-        return ;
-    if (camera->origin)
-        free_vector(camera->origin);
-    if (camera->direction)
-        free_vector(camera->direction);
-    free(camera);
+	if (!camera)
+		return ;
+	if (camera->origin)
+		free_vector(camera->origin);
+	if (camera->direction)
+		free_vector(camera->direction);
+	free(camera);
 }
--- src/scene/scene.c
+++ src/scene/scene.c
@@ -3,43 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   scene.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amitcul <amitcul@student.42porto.c>        +#+  +:+       +#+        */
+/*   By: amitcul <amitcul@student.42porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 16:47:05 by amitcul           #+#    #+#             */
-/*   Updated: 2023/06/23 16:47:05 by amitcul          ###   ########.fr       */
+/*   Updated: 2023/07/17 11:44:28 by amitcul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
 
-t_scene *new_scene(float width, float height)
+t_scene	*new_scene(float width, float height)
 {
-    t_scene *scene;
+	t_scene	*scene;
 
-    scene = (t_scene *) malloc(sizeof(t_scene));
-    scene->width = width;
-    scene->height = height;
+	scene = (t_scene *)malloc(sizeof(t_scene));
+	scene->width = width;
+	scene->height = height;
 	scene->ambient_light = NULL;
-    scene->light = NULL;
-    scene->camera = NULL;
-    scene->spheres = NULL;
-    scene->planes = NULL;
-    scene->cylinders = NULL;
-    return (scene);
+	scene->light = NULL;
+	scene->camera = NULL;
+	scene->spheres = NULL;
+	scene->planes = NULL;
+	scene->cylinders = NULL;
+	return (scene);
 }
 
-void free_scene(t_scene *scene)
+void	free_scene(t_scene *scene)
 {
 	if (!scene)
 		return ;
-    if (scene->light)
-        free_light(scene->light);
-    if (scene->ambient_light)
-        free_ambient_light(scene->ambient_light);
-    if (scene->camera)
-        free_camera(scene->camera);
-    free_sphere_list(scene->spheres);
-    free_plane_list(scene->planes);
-    free_cylinder_list(scene->cylinders);
-    free(scene);
+	if (scene->light)
+		free_light(scene->light);
+	if (scene->ambient_light)
+		free_ambient_light(scene->ambient_light);
+	if (scene->camera)
+		free_camera(scene->camera);
+	free_sphere_list(scene->spheres);
+	free_plane_list(scene->planes);
+	free_cylinder_list(scene->cylinders);
+	free(scene);
 }
